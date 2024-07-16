e93d27150e82772f61afccd1745b7bd369d0e9d8
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-07-16 11:59:13 +0600

    update

--- capital_to_small_small_to_capital.asm
+++ capital_to_small_small_to_capital.asm
@@ -1,69 +1,63 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    E_M DB "ERROR ! DIGIT ENTERED $", 0DH, 0AH, "$"
+    ERR_MSG DB 'Error: Digit entered!', 0DH, 0AH, '$'  ; Error message for digits
 .CODE
 MAIN PROC
-    
     MOV AH,01
-    INT 21H
-    MOV BL,AL
+    INT 21H         ; Read a character from the keyboard
+    MOV BL,AL       ; Store the character in BL
+    CMP BL, '0'     ; Check if the character is a digit (0-9)
+    JL CHECK_ALPHA  ; If less than '0', it's not a digit
+    CMP BL, '9'
+    JG CHECK_ALPHA  ; If greater than '9', it's not a digit
     
-    
-    CMP BL,'0'
-    JL CHECK_ALPHA
-    CMP BL,'9'
-    JG CHECK_ALPHA
-    
-    
-    MOV AH,2
-    MOV DL,0DH
-    INT 21H
-    MOV AH,2
-    MOV DL,0AH
-    INT 21H
-    
-    
-    MOV DX, OFFSET E_M
-    MOV AH,09
-    INT 21H
-    JMP EXIT
-    
-    
-    CHECK_ALPHA:
-    MOV AH,2
-    MOV DL,0DH
-    INT 21H
-    MOV AH,2
-    MOV DL,0AH
-    INT 21H
-    
-    CMP BL,'a'
-    JGE SMALL_TO_BIG
-    
-    BIG_TO_SMALL:
-    ADD BL, 32
+    ; Line break before displaying error message
     MOV AH,02
-    MOV DL, BL
-    INT 21H
-    JMP NEWLINE
-    
-    SMALL_TO_BIG:
-    SUB BL,32
+    MOV DL,0DH
+    INT 21H         ; Carriage return
+    MOV AH,02
+    MOV DL,0AH
+    INT 21H         ; Line feed
+
+    MOV DX, OFFSET ERR_MSG
+    MOV AH,09
+    INT 21H         ; Display the error message
+    JMP EXIT        ; Exit the program
+
+CHECK_ALPHA:
+    MOV AH,02
+    MOV DL,0DH
+    INT 21H         ; Carriage return
+    MOV AH,02
+    MOV DL,0AH
+    INT 21H         ; Line feed
+    CMP BL, 'a'
+    JGE SMALL_TO_BIG ; Check if the character is a lowercase letter
+
+BIG_TO_SMALL:
+    ADD BL, 32      ; Convert to lowercase
     MOV AH,02
     MOV DL,BL
     INT 21H
-    
-    NEWLINE:
+    JMP NEWLINE
+
+SMALL_TO_BIG:
+    SUB BL, 32      ; Convert to uppercase
+    MOV AH,02
+    MOV DL,BL
+    INT 21H
+
+NEWLINE:
     MOV AH,02
     MOV DL,0DH
-    INT 21H
+    INT 21H         ; Carriage return
     MOV AH,02
     MOV DL,0AH
-    INT 21H
-    
-    
-    
-    EXIT:
-    MAIN ENDP
+    INT 21H         ; Line feed
+
+EXIT:
+    MOV AH,4CH
+    INT 21H         ; Exit the program
+MAIN ENDP
 END MAIN
